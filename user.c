925a985cb5d856d607dfa863401c85d98dc10076
Sarah Leong <sleong20@stuy.edu>
2021-12-09 21:45:19 -0500

    i assume this should work on linux... but on windows it doesn't even make the pipe

--- user.c
+++ user.c
@@ -9,23 +9,26 @@
 #include <sys/types.h>
 
 int main() {
+    mkfifo("utow",0644);
+    mkfifo("wtou",0644);
+
+    int fromw = open("wtou",O_RDONLY);
+    int tow = open("utow",O_WRONLY);
+    printf("fromw: %d\ttow: %d\n",fromw,tow);
 
     char line[100];
     int e = 1;
 
     while (e) {        
         // copied from work18
-        close(ctop[WRITE]);
-        close(ptoc[READ]);
         printf("Enter input (exit to exit): ");
         fgets(line, 100, stdin);
 
         if (!strcmp(line, "exit\n")) {
             e = 0;
         } else {
-            write(ptoc[WRITE], line, 100);   
-            char line[100];
-            read(ctop[READ], line, 100);
+            write(tow, line, 100);
+            read(fromw, line, 100);
             printf("parent got from child: %s\n",line);
         }   
     }

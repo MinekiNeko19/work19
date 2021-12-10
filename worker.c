b79fabd19b02eb95a7bdb4f9ef7f42449c19a5f0
Sarah Leong <sleong20@stuy.edu>
2021-12-09 21:45:39 -0500

    idk b/c the pipe doesn't open on windows

--- worker.c
+++ worker.c
@@ -8,25 +8,25 @@
 #include <sys/stat.h>
 #include <sys/types.h>
 
-int main() {
+int main() {    
+    int fromu = open("wtou",O_WRONLY);
+    int tou = open("utow",O_RDONLY);
+
     char line[100];
     int e = 1;
 
     while (e) {
-
         // copied from work18
-        close(ctop[READ]);
-        close(ptoc[WRITE]);
-        read(ptoc[READ], line, 100);
+        read(tou, line, 100);
         if (!strcmp(line, "exit\n")) {
             e = 0;
-         else {
+        } else {
             int i = 0;
             while (line[i] != '\n') {
                 line[i] = toupper(line[i]);
                 i++;
             }
-            write(ctop[WRITE], line, 100);
+            write(fromu, line, 100);
         }
 
     return 0;
